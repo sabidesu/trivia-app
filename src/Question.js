80bbe437f480f0aa18d9eef8e11bb71b1a714826
julian donald <zentixzaneic@gmail.com>
2022-04-17 01:41:28 -0400

    add button component

--- src/Question.js
+++ src/Question.js
@@ -1,15 +1,21 @@
 import React from "react";
+import Button from "./Button.js";
 
 class Question extends React.Component {
 	constructor(props) {
 		super(props);
-		let isAnswered = false;
+	}
+
+	render() {
+		let buttons = [];
+		buttons.push(<Button correct={true} text={this.props.correct} clicked={false}/>);
+		this.props.wrong.forEach(answer => 
+			buttons.push(<Button correct={false} text={answer} clicked={false}/>));
+
+		return (
+			<div>{this.props.question}</div>
+		);
 	}
 }
 
-export function makeQuestion(info) {
-	console.log(info);
-	return info;
-}
-
-export default makeQuestion;
+export default Question;
--- src/Trivia.js
+++ src/Trivia.js
@@ -1,25 +1,40 @@
 import {useState, useEffect} from "react";
-import makeQuestion from "./Question.js";
+import Question from "./Question.js";
 
 function Trivia() {
 	let trivia_api = 'https://opentdb.com/api.php?amount=10';
+	const [haveJSON, setHaveJSON] = useState(false);
 	let qList = [];
 
 	useEffect(() => {
 		fetch(trivia_api)
 			.then(response => response.json())
-			.then(data => {
-				console.log(data);
-				qList = data.results.map((qInfo) => makeQuestion(qInfo));
+			.then(qJSON => {
+				console.log(qJSON);
+				qList = qJSON.results.map((qInfo) => makeQuestion(qInfo));
+				setHaveJSON(true);
 			})
 			.catch(error => console.error("error fetching questions: ", error))
-	}, [])
+	}, []);
 
 	return (
-		<>
+		<div>
 			{qList}
-		</>
-	)
+		</div>
+	);
+}
+
+function makeQuestion(info) {
+	console.log(info);
+
+	return (
+		<Question 
+			question={info.question} 
+			correct={info.correct_answer} 
+			wrong={info.incorrect_answers}
+			isAnswered={false}
+			isCorrect={false} />
+	);
 }
 
 export default Trivia;
