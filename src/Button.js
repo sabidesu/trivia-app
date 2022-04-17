e70abef80d50e86f9dce018808cd5b3b40fa26db
julian donald <zentixzaneic@gmail.com>
2022-04-17 01:46:43 -0400

    add text to button

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 class Button extends React.Component {
-	
+	constructor(props) {
+		super(props);
+	}	
+
+	render() {
+		return (
+			<button>{this.props.text}</button>
+		)
+	}
 }
 
 export default Button;
